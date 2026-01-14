cd98fcbcbab21bc21191d1a42c5a370126af67ed
aboulayetchakoura <aboulayet63@gmail.com>
2026-01-14 12:46:52 -0500

    added icons , rating and refracture the shopping and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,17 @@ export const Navbar = () => {
   return (
     <nav className="w-full bg-white shadow-md navbar">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-       <div class="amazon-header-right-section">
+       <div className="amazon-header-right-section">
 
-        <a class="orders-link header-link" href="orders.html">
-          <span class="returns-text">Returns</span>
-          <span class="orders-text">& Orders</span>
+        <a className="orders-link header-link" href="orders.html">
+          <span className="returns-text">Returns</span>
+          <span className="orders-text">& Orders</span>
         </a>
 
-        <a class="cart-link header-link" href="checkout.html">
-          <img class="cart-icon" src="images/icons/cart-icon.png" alt="Shopping Cart" />
-          <div class="cart-quantity">3</div>
-          <div class="cart-text">Cart</div>
+        <a className="cart-link header-link" href="checkout.html">
+          <img className="cart-icon" src="./assets/icons/cart-icon.png" alt="cCart" />
+          <div className="cart-quantity">3</div>
+          <div className="cart-text">Cart</div>
         </a>
       </div>
         
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,31 +1,15 @@
-export const shoppinCart = () => {
+export const ShoppingCart = () => {
   return (
    <>
    <div className="amazon-header">
-   <div className="amazon-header-left-section">
-    <a href="" className="cart-link header-link">
-        <img src="images/icons/cart-icon.png" alt="Shopping Cart" />
-    </a>
-   </div>
-   <div class="amazon-header-middle-section">
+   <div className="amazon-header-middle-section">
         <input className="search-bar" type="text" placeholder="Search"/>
 
-        <button class="search-button">
-          <img class="search-icon" src="images/icons/search-icon.png"/>
+        <button className="search-button">
+          <img className="search-icon" src="images/icons/search-icon.png"/>
         </button>
       </div>
-      <div className="amazon-header-right-section">
-        <a className="orders-link header-link" href="orders.html">
-          <span className="returns-text">Returns</span>
-          <span className="orders-text">& Orders</span>
-        </a>
-
-        <a class="cart-link header-link" href="checkout.html">
-          <img className="cart-icon" src="images/icons/cart-icon.png"/>
-          <div className="cart-quantity">3</div>
-          <div className="cart-text">Cart</div>
-        </a>
-      </div>
+     
       </div>
       <div className="main">
       <div className="products-grid">
