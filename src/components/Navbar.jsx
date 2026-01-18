814b0af052d8559a18056d3ae20cd4b37179546f
aboulayetchakoura <aboulayet63@gmail.com>
2026-01-18 14:08:02 -0500

    done navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,20 @@ import { Input } from './Input';
 
 export const Navbar = () => {
   return (
-    <nav className="w-full bg-blue-500 shadow-md navbar flex items-center text-white pw-4 py-2 justify-between">
+    <nav className=" bg-blue-500 shadow-md navbar flex items-center text-white justify-between px-6 py-2">
 
       < Input />
-       <div className="amazon-header-right-section flex items-center gap-6  ">
-          <a className="orders-link header-link hover:border-white" href="orders.html">
+       <div className="amazon-header-right-section flex items-center gap-6 ">
+          <a className="orders-link header-link border-2 border-blue-500 hover:border-gray-200 transition-colors duration-300 p-4" href="orders.html">
             <span className="returns-text">Returns</span>
             <span className="orders-text">& Orders</span>
           </a>
 
-          <a className=" flex  cart-link header-link" href="checkout.html">
+          <a className=" flex  cart-link header-link border-2 border-blue-500 hover:border-gray-200 transition-colors duration-300 p-4 " href="checkout.html">
             <div className="cart-icon-container relative flex items-center">
-            <img className="cart-icon" src="images//icons/cart-icon.png" alt="cart" />
-            <div className=" absolute top-2 left-10 cart-quantity text-lg">3</div>
-            <div className="cart-text ">Cart</div>
+              <img className="cart-icon" src="images//icons/cart-icon.png" alt="cart" />
+              <div className=" absolute top-2 left-10 cart-quantity text-lg">3</div>
+              <div className="cart-text ">Cart</div>
             </div>
           </a>
       </div>
