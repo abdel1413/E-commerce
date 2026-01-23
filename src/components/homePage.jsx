80552215522ec98e8509458fd755e7f6b597e6ab
aboulayetchakoura <aboulayet63@gmail.com>
2026-01-23 11:51:56 -0500

    add rating to products image

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -18,6 +18,7 @@ return (
   
      { products.map((product) => (
     
+      
       <div
          className="border border-gray-200 rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow duration-300"
          key={product.id}>
@@ -27,8 +28,14 @@ return (
           alt={product.name}
           className="w-full h-48 object-cover mb-4 rounded img-product"
         />
+        {console.log(product.rating.stars *10)}
         <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
-        <p className="text-gray-600 mb-4">${product.priceCents}</p>
+        <p className="text-gray-600 mb-4">${(product.priceCents/100).toFixed(2)}</p>
+        <div className="flex gap-2 item-center ">
+        
+        <img className="ratings w-32 h-5 text-blue-500" src={`/images/ratings/rating-${product.rating.stars*10}.png`}/> <span>{product.rating.count}</span>
+        </div>
+           
            <DropDown />
         <button className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition-colors duration-300">
           Add to Cart
