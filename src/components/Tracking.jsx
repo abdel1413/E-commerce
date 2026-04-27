34faaf1547f1f539298f7292181ce1f51ae55b0b
aboulayetchakoura <aboulayet63@gmail.com>
2026-04-27 17:16:58 -0400

    created and style tracking item

--- src/components/Tracking.jsx
+++ src/components/Tracking.jsx
@@ -16,13 +16,27 @@ export const Tracking =({orders })=>{
    
 
     let trackingOrder = savedOrders[0].items.find(item => item.id ===(orderId.id) ) 
-    console.log('tracking order', trackingOrder)
+    console.log('tracking order', trackingOrder.name, trackingOrder.quantity, trackingOrder.image )
     const {name, image, quantity} = trackingOrder
+    console.log('img',image)
 
     const deliveryDate = (savedOrders.map(order => order.estimatedDeliveryTime))
    const formattedDeliveryDate = dayjs(deliveryDate[0]).format("dddd, MMMM D")
 
 
+
+   //create progress bar logic based on order status
+   const now = dayjs().valueOf()
+  
+   const order = savedOrders[0]
+
+
+   const {orderDate, estimatedDeliveryTime} = order
+   const totalTime = estimatedDeliveryTime - orderDate
+
+   const elapsedTime = now - orderDate
+
+   const progressPercentage = Math.min((elapsedTime / totalTime) * 100, 100)  
    
 
     return (
@@ -34,6 +48,8 @@ export const Tracking =({orders })=>{
           View all orders
         </Link>
 
+   
+    <div className="container mt-6 p-6 bg-gray-100 rounded-lg shadow-lg mb-10">
         <div className="delivery-date text-xl text-bold-500 ">
           Arriving on {formattedDeliveryDate}
         </div>
@@ -48,9 +64,10 @@ export const Tracking =({orders })=>{
           Quantity: {quantity || 1}
         </div>
 
-        <img className="product-image w-32 h-32 object-cover rounded p-0 "
-        src={image} alt={name}  />
-        
+        <img className="product-image w-40 h-40 object-cover rounded p-0  border border-gray-300  shadow: shadow-lg dark: text-black "
+        src={`/${image}`}/>
+
+        </div>
 
         <div className="progress-labels-container flex justify-between">
           <div className="progress-label">
@@ -65,7 +82,7 @@ export const Tracking =({orders })=>{
         </div>
 
         <div className="progress-bar-container w-full max-w-md h-5 bg-gray-200 rounded-full overflow-hidden mt-2 ">
-          <div className=" w-full progress-bar h-full bg-blue-300 transition-all duration-300" style={{width:"50%"}}>
+          <div className=" w-full progress-bar h-full bg-blue-300 transition-all duration-300" style={{width: `${progressPercentage}%`}}>
           </div>
         </div>
        
